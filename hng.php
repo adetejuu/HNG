5a88711d61ba3855f9367e9e43e224e9937b481f
adeteju <adeleyeadeteju@yahoo.com>
2018-04-10 09:26:38 +0100

    my stage one files

---
+++ hng.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head> 
+    <style> 
+        body {background-color: red}
+        h1 {color: black}
+        </style>
+    </head>
+<body>
+<h1> HELLO WORLD  </h1>
+ <?php
+date_default_timezone_set("Africa/Lagos");
+echo "Time in Nigeria: " . date("h:ia");
+?>
+    </body>
+</html>
